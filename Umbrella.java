3e7a0ed32d5cbf70620fc6b17ca3fd7ad5edd357
NAVEEN KUMAR <swatik.xworkz@gmail.com>
2022-09-11 23:03:53 +0530

    message test

---
+++ Umbrella.java
@@ -0,0 +1,43 @@
+class Umbrella{
+	static void colours(){
+		String colours="white,yellow,red";
+		System.out.println("Umbrella colours :"+ colours);
+	
+	}
+	static void buttons(){
+		int buttons =3;
+		System.out.println("Umbrella press buttons :"+ buttons);
+		
+	}
+	static void open(){
+		boolean open= true;
+		System.out.println("Umbrella is opening :"+ open);
+	}
+	static void close(){
+		boolean close= false;
+		System.out.println("Umbrella is closing:"+ close);
+	
+	}
+	static void ribs(){
+		int ribs =9;
+		System.out.println("No of  ribs:"+ ribs);
+	}
+	static void nylon(){
+		String nylon="clothe";
+		System.out.println("Umbrella is a nylon:"+nylon);
+		
+	}
+	static void length(){
+		float length=3.5f;
+		System.out.println("Umbrella length is:"+ length);
+		
+	}
+	static void width(){
+		float width=2.6f;
+		System.out.println("Umbrella width is :"+ width);
+	}
+	static void unscrew (){
+		byte unscrew=1;
+		System.out.println("unscrew the top of the umbrella is:"+ unscrew);
+	}
+}
