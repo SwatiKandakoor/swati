e5a17cd647c88167d654ccb9530e1294c23f9b8a
swatik.xworkz@gmail.com <swatik.xworkz@gmail.com>
2022-09-14 18:01:37 +0530

    identity file

---
+++ Identity.java
@@ -0,0 +1,58 @@
+class Identity{
+	static void printName(String Name){
+		
+		System.out.println("Identity printhName");
+		System.out.println("Name :"+ Name);
+	}
+	static void printNameAndEmail(String name, String emailid){
+		
+		System.out.println("enter the name and email");
+		System.out.println("Name:"+ name);
+		System.out.println("Email:"+ emailid);
+	}
+	static void printNameAndMobile(String name, long mobile){
+		System.out.println("Names and Mobile no");
+		System.out.println("Name :"+name);
+		System.out.println("Mobile no:"+mobile);
+		
+	}
+	static void printNameAndEmailAndAddressAndAge(String name, String email, String address, int age){
+		System.out.println("Name;"+name);
+		System.out.println("Email Id:"+email);
+		System.out.println("Address :"+address);
+		System.out.println("Age:"+age);
+	}
+    	static void printNameAndAge(String name,int age){
+		System.out.println("Enter the names and ages");
+		System.out.println("Name:"+name);
+		System.out.println("Age:"+age);
+	}
+	    static void printNameAndAddress(String name ,String address){
+		System.out.println("Enter the names and address");
+		System.out.println("Name:"+name);
+		System.out.println("Address:"+address);
+		
+	}
+		static void printNameAndGender(String name, char gender){
+			System.out.println("Name:"+name);
+			System.out.println("Gender:"+gender);
+	}
+	    static void printNameAndWorking(String name, int working){
+			System.out.println("Name:"+name);
+			System.out.println("Working:"+working);
+	}
+        
+	    static void printNameAndShirtSize(String name, char shirtsize){
+			System.out.println("Name:"+name);
+			System.out.println("Shirtsize:"+shirtsize);
+	}
+         static void printNameAndPantSize(String name, char pantsize){
+			System.out.println("Name:"+name);
+			System.out.println("pantsize:"+pantsize);
+	}
+	     static void printNameAndWeightAndHeight(String name, float weight,float height){
+			System.out.println("Name:"+name);
+			System.out.println("Weight:"+weight);
+			System.out.println("Height:"+height);
+		 }
+}
---
+++ IdentityRunner.java
@@ -0,0 +1,313 @@
+class IdentityRunner{
+	public static void main(String[] args){
+		String Value ="Vijay";
+		String value1="Naveen";
+		String value2="kavith";
+		String value3="Nayana";
+		String value4="Bhoomi";
+		String value5="Vani";
+		String value6="Nivetha";
+		String value7="kavya";
+		String value8="Sunitha";
+		String value9="Lakshmi";
+		String value10="Sahana";
+		String value11="jyoo";
+		
+		String name="Suraj S";	
+		String email="surajs.xworkz@gmail.com";
+		String name1="Sandeep s sonnad";
+        String email1="sandeepssonnadxworks@gmail.com";
+        String name2="Karthik. N";
+		String email2="karthikn.xworkz@gmail.com";
+        String name3= "chidanand s patroti";
+		String email3="chidanandxworkz@gmail.com";
+        String name4="Rohan R P";
+		String email4="rohanp.xworkz@gmail.com";
+        String name5="Anil Kumar D";
+		String email5="anil.xworkz@gmail.com";
+        String name6 ="Hithesh kumar";
+		String email6="hithesh.xworkz@gmail.com";
+		String name7 ="Prasad M";
+		String email7="prasadm.xworkz@gmail.com";
+        String name8="Manjunath shetty";
+		String email8="manjunaths.xworkz@gmail.com";
+        String name9="Ravi Waddar ";
+		String email9="raviwaddar.xworkz@gmail.com"; 
+        String name10="Gagan S V";
+		String email10="gagansv.xworkz@gmail.com";
+        String name11= " Vinayak R N";
+        String email11="vinayrn.xworkz@gmail.com";
+		String name12="sadanand ullagaddi";
+		String email12="sadanandu.xworkz@gmail.com";
+		
+		
+	    long no=7019336631L;
+        long no1=7259303408L;
+	    long no2=9741426746L;
+        long no3=6362060352L;
+        long no4 =8310437144L;
+        long no5=8088245073L;
+	    long no6=9380894119L;
+        long no7=9535044021L;
+        long no8=7795310346L;
+        long no9=9071177871L;
+	    long no10=2349584878L;
+	    long no11=7996258738L;
+		
+		
+		String address="fhfgoiuddkldfkfdjofdkl";
+		int age =24;
+		String address1="fhfgoiuddkldfkfdjofdkl";
+		int age1 =22;
+		String address2="fhfgoiuddkldfkfdjofdkl";
+		int age2=24;
+		String address3="fhfgoiuddkldfkfdjofdkl";
+		int age3 =23;
+		String address4="fhfgoiuddkldfkfdjofdkl";
+		int age4 =24;
+		String address5="fhfgoiuddkldfkfdjofdkl";
+		int age5 =23;
+		String address6="fhfgoiuddkldfkfdjofdkl";
+		int age6 =23;
+		String address7="fhfgoiuddkldfkfdjofdkl";
+		int age7 =22;
+		String address8="fhfgoiuddkldfkfdjofdkl";
+		int age8 =23;
+		String address9="fhfgoiuddkldfkfdjofdkl";
+		int age9 =24;
+		String address10="fhfgoiuddkldfkfdjofdkl";
+		int age10 =22;
+		String address11="fhfgoiuddkldfkfdjofdkl";
+		int age11 =21;
+		String address12="fhfgoiuddkldfkfdjofdkl";
+		int age12 =24;
+		
+		char gender='F';
+		char gender1='M';
+		char gender2='M';
+		char gender3='F';
+		char gender4='F';
+		char gender5='M';
+		char gender6='M';
+		char gender7='F';
+		char gender8='M';
+		char gender9='F';
+		char gender10='M';
+		char gender11='F';
+		
+		int working=2;
+		int working1=6;
+		int working2=10;
+		int working3=12;
+		int working4=10;
+		int working5=7;
+		int working6=8;
+		int working7=4;
+		int working8=9;
+		int working9=5;
+		int working10=1;
+		int working11=3;
+		
+	    char shirtsize='L';
+		char shirtsize1='M';
+		char shirtsize2='S';
+		char shirtsize3='L';
+		char shirtsize4='L';
+		char shirtsize5='S';
+		char shirtsize6='S';
+		char shirtsize7='L';
+		char shirtsize8='L';
+		char shirtsize9='M';
+		char shirtsize10='S';
+		char shirtsize11='M';
+		
+	    char pantsize='L';
+		char pantsize1='M';
+		char pantsize2='S';
+		char pantsize3='L';
+		char pantsize4='L';
+		char pantsize5='S';
+		char pantsize6='S';
+		char pantsize7='L';
+		char pantsize8='L';
+		char pantsize9='M';
+		char pantsize10='S';
+		char pantsize11='M';
+		
+		float height=5.5f;
+		float weight=67f;
+		float height1=5.2f;
+		float weight1=62f;
+		float height2=4.9f;
+		float weight2=48.8f;
+		float height3=4.6f;
+		float weight3=36.1f;
+		float height4=5.1f;
+		float weight4=45f;
+		float height5=4.8f;
+		float weight5=50f;
+		float height6=5.9f;
+		float weight6=67.9f;
+		float height7=4.2f;
+		float weight7=41f;
+		float height8=4.3f;
+		float weight8=39.2f;
+		float height9=4.9f;
+		float weight9=42f;
+		float height10=5.2f;
+		float weight10=43.5f;
+		float height11=5.3f;
+		float weight11=56.5f;
+		
+		Identity.printName(Value);
+		Identity.printName(value1);
+		Identity.printName(value2);
+		Identity.printName(value3);
+		Identity.printName(value4);
+	    Identity.printName(value5);
+		Identity.printName(value6);
+	    Identity.printName(value7);
+		Identity.printName(value8);
+	    Identity.printName(value9);
+		Identity.printName(value10);
+	    Identity.printName(value11);
+		
+		Identity.printNameAndEmail(name,email);
+		Identity.printNameAndEmail(name1,email1);
+		Identity.printNameAndEmail(name2,email2);
+		Identity.printNameAndEmail(name3,email3);
+		Identity.printNameAndEmail(name4,email4);
+		Identity.printNameAndEmail(name5,email5);
+		Identity.printNameAndEmail(name6,email6);
+		Identity.printNameAndEmail(name7,email7);
+		Identity.printNameAndEmail(name8,email8); 
+		Identity.printNameAndEmail(name9,email9); 
+		Identity.printNameAndEmail(name10,email10); 
+		Identity.printNameAndEmail(name11,email1);
+		Identity.printNameAndEmail(name12,email12);	
+		
+		Identity.printNameAndMobile(name,no);
+	    Identity.printNameAndMobile(name1,no1);
+	    Identity.printNameAndMobile(name2,no2);
+	    Identity.printNameAndMobile(name3,no3);
+	    Identity.printNameAndMobile(name4,no4);
+	    Identity.printNameAndMobile(name5,no5);
+	    Identity.printNameAndMobile(name6,no6);
+	    Identity.printNameAndMobile(name7,no7);
+	    Identity.printNameAndMobile(name8,no8);
+	    Identity.printNameAndMobile(name9,no9);
+	    Identity.printNameAndMobile(name10,no10);
+	    Identity.printNameAndMobile(name11,no11);
+	    
+		
+		Identity.printNameAndEmailAndAddressAndAge(name,email,address,age);
+		Identity.printNameAndEmailAndAddressAndAge(name1,email1,address1,age1);
+	    Identity.printNameAndEmailAndAddressAndAge(name2,email2,address2,age2);
+	    Identity.printNameAndEmailAndAddressAndAge(name3,email3,address3,age3);
+	    Identity.printNameAndEmailAndAddressAndAge(name4,email4,address4,age4);
+	    Identity.printNameAndEmailAndAddressAndAge(name5,email5,address5,age5);
+	    Identity.printNameAndEmailAndAddressAndAge(name6,email6,address6,age6);
+	    Identity.printNameAndEmailAndAddressAndAge(name7,email7,address7,age7);
+		Identity.printNameAndEmailAndAddressAndAge(name1,email8,address8,age8);
+	    Identity.printNameAndEmailAndAddressAndAge(name2,email9,address9,age9);
+	    Identity.printNameAndEmailAndAddressAndAge(name3,email10,address10,age10);
+	    Identity.printNameAndEmailAndAddressAndAge(name4,email11,address11,age11);
+	    Identity.printNameAndEmailAndAddressAndAge(name5,email12,address12,age12);
+		
+	    Identity.printNameAndAge(Value,age);
+		Identity.printNameAndAge(value1,age1);
+		Identity.printNameAndAge(value2,age2);
+		Identity.printNameAndAge(value3,age3);
+		Identity.printNameAndAge(value4,age4);
+	    Identity.printNameAndAge(value5,age5);
+		Identity.printNameAndAge(value6,age6);
+	    Identity.printNameAndAge(value7,age7);
+		Identity.printNameAndAge(value8,age8);
+	    Identity.printNameAndAge(value9,age9);
+		Identity.printNameAndAge(value10,age10);
+	    Identity.printNameAndAge(value11,age11);
+		
+		
+		Identity.printNameAndAddress(Value,address);
+		Identity.printNameAndAddress(value1,address1);
+		Identity.printNameAndAddress(value2,address2);
+		Identity.printNameAndAddress(value3,address3);
+		Identity.printNameAndAddress(value4,address4);
+	    Identity.printNameAndAddress(value5,address5);
+		Identity.printNameAndAddress(value6,address6);
+	    Identity.printNameAndAddress(value7,address7);
+		Identity.printNameAndAddress(value8,address8);
+	    Identity.printNameAndAddress(value9,address9);
+		Identity.printNameAndAddress(value10,address10);
+	    Identity.printNameAndAddress(value11,address11);
+	
+	    Identity.printNameAndGender(Value,gender);
+		Identity.printNameAndGender(value1,gender1);
+		Identity.printNameAndGender(value2,gender2);
+		Identity.printNameAndGender(value3,gender3);
+		Identity.printNameAndGender(value4,gender4);
+	    Identity.printNameAndGender(value5,gender5);
+		Identity.printNameAndGender(value6,gender6);
+	    Identity.printNameAndGender(value7,gender7);
+		Identity.printNameAndGender(value8,gender8);
+	    Identity.printNameAndGender(value9,gender9);
+		Identity.printNameAndGender(value10,gender10);
+	    Identity.printNameAndGender(value11,gender11);
+		
+		Identity.printNameAndWorking(Value,working);
+		Identity.printNameAndWorking(value1,working1);
+		Identity.printNameAndWorking(value2,working2);
+		Identity.printNameAndWorking(value3,working3);
+		Identity.printNameAndWorking(value4,working4);
+	    Identity.printNameAndWorking(value5,working5);
+		Identity.printNameAndWorking(value6,working6);
+	    Identity.printNameAndWorking(value7,working7);
+		Identity.printNameAndWorking(value8,working8);
+	    Identity.printNameAndWorking(value9,working9);
+		Identity.printNameAndWorking(value10,working10);
+	    Identity.printNameAndWorking(value11,working11);
+		
+		Identity.printNameAndShirtSize(Value,shirtsize);
+		Identity.printNameAndShirtSize(value1,shirtsize1);
+		Identity.printNameAndShirtSize(value2,shirtsize2);
+		Identity.printNameAndShirtSize(value3,shirtsize3);
+		Identity.printNameAndShirtSize(value4,shirtsize4);
+	    Identity.printNameAndShirtSize(value5,shirtsize5);
+		Identity.printNameAndShirtSize(value6,shirtsize6);
+	    Identity.printNameAndShirtSize(value7,shirtsize7);
+		Identity.printNameAndShirtSize(value8,shirtsize8);
+	    Identity.printNameAndShirtSize(value9,shirtsize9);
+		Identity.printNameAndShirtSize(value10,shirtsize10);
+	    Identity.printNameAndShirtSize(value11,shirtsize11);
+	
+	    Identity.printNameAndPantSize(Value,pantsize);
+		Identity.printNameAndPantSize(value1,pantsize1);
+		Identity.printNameAndPantSize(value2,pantsize2);
+		Identity.printNameAndPantSize(value3,pantsize3);
+		Identity.printNameAndPantSize(value4,pantsize4);
+	    Identity.printNameAndPantSize(value5,pantsize5);
+		Identity.printNameAndPantSize(value6,pantsize6);
+	    Identity.printNameAndPantSize(value7,pantsize7);
+		Identity.printNameAndPantSize(value8,pantsize8);
+	    Identity.printNameAndPantSize(value9,pantsize9);
+		Identity.printNameAndPantSize(value10,pantsize10);
+	    Identity.printNameAndPantSize(value11,pantsize11);
+		
+		
+		Identity.printNameAndWeightAndHeight(Value,weight,height);
+		Identity.printNameAndWeightAndHeight(value1,weight1,height1);
+		Identity.printNameAndWeightAndHeight(value2,weight2,height2);
+		Identity.printNameAndWeightAndHeight(value3,weight3,height3);
+		Identity.printNameAndWeightAndHeight(value4,weight4,height4);
+	    Identity.printNameAndWeightAndHeight(value5,weight5,height5);
+		Identity.printNameAndWeightAndHeight(value6,weight6,height6);
+	    Identity.printNameAndWeightAndHeight(value7,weight7,height7);
+		Identity.printNameAndWeightAndHeight(value8,weight8,height8);
+	    Identity.printNameAndWeightAndHeight(value9,weight9,height9);
+		Identity.printNameAndWeightAndHeight(value10,weight10,height10);
+	    Identity.printNameAndWeightAndHeight(value11,weight11,height11);
+	
+	
+	
+	}
+}
