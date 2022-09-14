dbb1365ca0519b78d83a0d1bdec4d1ef1ba12092
swatik.xworkz@gmail.com <swatik.xworkz@gmail.com>
2022-09-14 23:35:34 +0530

    states

---
+++ States.java
@@ -0,0 +1,122 @@
+class States{
+    static String getCapitalByState(String state){
+		
+		System.out.println("getCapitalByState");
+		 
+		 if (state=="Karnataka"){
+			 System.out.println(state);
+			 return "Bengaluru";
+		 }
+		 if (state=="Andhra Pradesh"){
+			 System.out.println(state);
+			 return "Hyderbad";
+		 }
+		 if (state=="Arunachal Pradesh"){
+			 System.out.println(state);
+			 return "Itanagar";
+		 }
+		 if (state=="Assam"){
+			 System.out.println(state);
+			 return "Dispur";
+		 }
+		 if (state=="Bihar"){
+			 System.out.println(state);
+			 return "Patna";
+		 }
+		 if (state=="Chhattisgarh"){
+			 System.out.println(state);
+			 return "Raipur";
+		 }
+		 if (state=="Goa"){
+			 System.out.println(state);
+			 return "Panaji";
+		 }
+		 if (state=="Gujarat"){
+			 System.out.println(state);
+			 return "Gandhinagar";
+		 }
+		 if (state=="Haryana"){
+			 System.out.println(state);
+			 return "Chandigarh";
+		 }
+		 if (state=="Himachal Pradesh"){
+			 System.out.println(state);
+			 return "Shimla";
+		 }
+		 if (state=="Jharkhand"){
+			 System.out.println(state);
+			 return "Ranchi";
+		 }
+		 if (state=="Kerala"){
+			 System.out.println(state);
+			 return "Thiruvananthauram";
+		 }
+		 if (state=="Madhya Pradesh"){
+			 System.out.println(state);
+			 return "Bhopal";
+		 }
+		 if (state=="Maharashtra"){
+			 System.out.println(state);
+			 return "Mumbai";
+		 }
+		 if (state=="Manipur"){
+			 System.out.println(state);
+			 return "Imphal";
+		 }
+		 if (state=="Meghalaya"){
+			 System.out.println(state);
+			 return "Shillong";
+		 }
+		 if (state=="Mizoram"){
+			 System.out.println(state);
+			 return "Aizawl";
+		 }
+		 if (state=="Nagaland"){
+			 System.out.println(state);
+			 return "Kohima";
+		 }
+		 if (state=="Odisha"){
+			 System.out.println(state);
+			 return "Bhubaneswar";
+		 }
+		 if (state=="Punjab"){
+			 System.out.println(state);
+			 return "Chandigarh";
+		 }
+		 if (state=="Rajasthan"){
+			 System.out.println(state);
+			 return "Jaipur";
+		 }
+		 if (state=="Sikkim"){
+			 System.out.println(state);
+			 return "Gangtok";
+		 }
+		 if (state=="Tamil Nadu"){
+			 System.out.println(state);
+			 return "Chennai";
+		 }
+		 if (state=="Telangana"){
+			 System.out.println(state);
+			 return "Hyderabad";
+		 }
+		 if (state=="Tripura"){
+			 System.out.println(state);
+			 return "Agartala";
+		 }
+		 if (state=="Uttar Pradesh"){
+			 System.out.println(state);
+			 return "Lucknow";
+		 }
+		 if (state=="Uttarakhand"){
+			 System.out.println(state);
+			 return "Dehradun";
+		 }
+		 if (state=="West Bengal"){
+			 System.out.println(state);
+			 return "Kolkata";
+		 }
+		 
+return null;		 
+	}
+ 
+}
---
+++ StatesRunner.java
@@ -0,0 +1,92 @@
+class StatesRunner{
+	public static void main(String [] args){
+		System.out.println("Entered States and Capital");
+		
+		String ref=States.getCapitalByState("Karnataka");
+		System.out.println(ref);
+		
+		String ref1=States.getCapitalByState("Andhra Pradesh");
+		System.out.println(ref1);
+		
+		String ref2=States.getCapitalByState("Arunachal Pradesh");
+		System.out.println(ref2);
+		
+		String ref3=States.getCapitalByState("Assam");
+		System.out.println(ref3);
+		
+		String ref4=States.getCapitalByState("Bihar");
+		System.out.println(ref4);
+		
+		String ref5=States.getCapitalByState("Chhattisgarh");
+		System.out.println(ref5);
+		
+		String ref6=States.getCapitalByState("Goa");
+		System.out.println(ref6);
+		
+		String ref7=States.getCapitalByState("Gujarat");
+		System.out.println(ref7);
+		
+		String ref8=States.getCapitalByState("Haryana");
+		System.out.println(ref8);
+		
+		String ref9=States.getCapitalByState("Himachal Pradesh");
+		System.out.println(ref9);
+		
+		String ref10=States.getCapitalByState("Jharkhand");
+		System.out.println(ref10);
+		
+		String ref11=States.getCapitalByState("Kerala");
+		System.out.println(ref11);
+		
+		String ref12=States.getCapitalByState("Madhya Pradesh");
+		System.out.println(ref12);
+		
+		String ref13=States.getCapitalByState("Maharashtra");
+		System.out.println(ref13);
+		
+		String ref14=States.getCapitalByState("Manipur");
+		System.out.println(ref14);
+		
+		String ref15=States.getCapitalByState("Meghalaya");
+		System.out.println(ref15);
+		
+		String ref16=States.getCapitalByState("Mizoram");
+		System.out.println(ref16);
+		
+		String ref17=States.getCapitalByState("Nagaland");
+		System.out.println(ref17);
+		
+		String ref18=States.getCapitalByState("Odisha");
+		System.out.println(ref18);
+		
+		String ref19=States.getCapitalByState("Punjab");
+		System.out.println(ref19);
+		
+		String ref20=States.getCapitalByState("Rajasthan");
+		System.out.println(ref20);
+		
+		String ref21=States.getCapitalByState("Sikkim");
+		System.out.println(ref21);
+		
+		String ref22=States.getCapitalByState("Tamil Nadu");
+		System.out.println(ref22);
+		
+		String ref23=States.getCapitalByState("Telangana");
+		System.out.println(ref23);
+		
+		String ref24=States.getCapitalByState("Tripura");
+		System.out.println(ref24);
+		
+		String ref25=States.getCapitalByState("Uttar Pradesh");
+		System.out.println(ref25);
+		
+		String ref26=States.getCapitalByState("Uttarakhand");
+		System.out.println(ref26);
+		
+		String ref27=States.getCapitalByState("West Bengal");
+		System.out.println(ref27);
+		
+	}
+	
+	
+}
