b97cf69a45139600c5d6f4dbba9f63d11408d9d0
swatik.xworkz@gmail.com <swatik.xworkz@gmail.com>
2022-09-14 22:14:51 +0530

    mobile numbers

---
+++ MobileNo.java
@@ -0,0 +1,64 @@
+class MobileNo{
+	static  String getNameByMobile(long number)
+	{
+		 System.out.println("getNameByMobile");
+		 
+		 if (number==7019336631L){
+			 System.out.println(number);
+			 return "Shilpa";
+		 }		
+			
+		  if (number==8073982483L){
+			 System.out.println(number);
+			 return "Swati";
+		 
+		 }
+			
+		  if (number==9980361822L){
+			 System.out.println(number);
+			 return "Swatikandakkor";
+			 
+		  }
+			
+		  if (number==9731631911L){
+			 System.out.println(number);
+			 return "Naveen";
+		  } 
+			
+		  if (number==8976543256L){
+			 System.out.println(number);
+			 return "vijay";
+		  }
+			
+		  if (number==9967543267L){
+			 System.out.println(number);
+			 return "kavitha";
+		  }
+			
+		  if (number==7869675446L){
+			 System.out.println(number);
+			 return "vani";
+		  } 
+			
+		  if (number==8976543228L){
+			 System.out.println(number);
+			 return "Kavya";
+		  } 
+		  
+		 
+		   if (number==9876754332L){
+			 System.out.println(number);
+			 return "Sunitha";
+		  } 
+		  
+		   if (number==9876543326L){
+			 System.out.println(number);
+			 return "Nivi";
+		  } 
+		 
+		 
+return null;		 
+	} 
+	
+	
+}
---
+++ MobileNoRunner.java
@@ -0,0 +1,35 @@
+class MobileNoRunner{
+	public static void main(String [] args){
+		System.out.println("Entered Constants");
+		
+		String ref=MobileNo.getNameByMobile(7019336631L);
+		System.out.println(ref);
+	
+		String ref1=MobileNo.getNameByMobile(8073982483L);
+		System.out.println(ref1);
+		
+		String ref2=MobileNo.getNameByMobile(9980361822L);
+		System.out.println(ref2);
+		
+		String ref3=MobileNo.getNameByMobile(9731631911L);
+		System.out.println(ref3);
+		
+		String ref4=MobileNo.getNameByMobile(8976543256L);
+		System.out.println(ref4);
+		
+		String ref5=MobileNo.getNameByMobile(9967543267L);
+		System.out.println(ref5);
+		
+		String ref6=MobileNo.getNameByMobile(7869675446L);
+		System.out.println(ref6);
+		
+		String ref7=MobileNo.getNameByMobile(8976543228L);
+		System.out.println(ref7);
+		
+		String ref8=MobileNo.getNameByMobile(9876754332L);
+		System.out.println(ref8);
+		
+		String ref9=MobileNo.getNameByMobile(9876543326L);
+		System.out.println(ref9);
+	}
+}
